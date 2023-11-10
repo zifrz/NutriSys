aac634362923ad40b7f39242b411b502465a6cb0
Ritesh Bakare <99970218+RiteshBakare@users.noreply.github.com>
2023-11-10 11:58:47 +0530

    API Instance updated with Gson || JSON

--- app/src/main/java/com/nutrisysteam/nutrisys/api/RetrofitInstance.kt
+++ app/src/main/java/com/nutrisysteam/nutrisys/api/RetrofitInstance.kt
@@ -1,5 +1,7 @@
 package com.nutrisysteam.nutrisys.api
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -7,10 +9,14 @@ object RetrofitInstance {
 
     private const val BASE_URL = "https://nutrition-recommendation-system-api.onrender.com"
 
+    private val gson: Gson = GsonBuilder()
+        .setLenient()
+        .create()
+
     val api: ApiInterface by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
             .create(ApiInterface::class.java)
     }
