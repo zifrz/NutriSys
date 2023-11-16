b018c48af04e178eb7bc815877de3b5cdcb1cd57
zifrz <114908186+zifrz@users.noreply.github.com>
2023-11-17 03:03:59 +0530

    Weekly Exercise UI using TableLayout

--- app/src/main/java/com/nutrisysteam/nutrisys/DetailExerciseActivity.kt
+++ app/src/main/java/com/nutrisysteam/nutrisys/DetailExerciseActivity.kt
@@ -7,5 +7,6 @@ class DetailExerciseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_exercise)
+
     }
 }
