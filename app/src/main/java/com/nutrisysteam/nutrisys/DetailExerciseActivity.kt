9716a0d9e25175d5523d1f3c5fc3530afc2b9dab
Ritesh Bakare <99970218+RiteshBakare@users.noreply.github.com>
2023-11-19 10:42:27 +0530

    Detail Activity Linked to Weekly Activity and the Equavalent Value is Passed to next Activity

--- app/src/main/java/com/nutrisysteam/nutrisys/DetailExerciseActivity.kt
+++ app/src/main/java/com/nutrisysteam/nutrisys/DetailExerciseActivity.kt
@@ -2,11 +2,29 @@ package com.nutrisysteam.nutrisys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.nutrisysteam.nutrisys.databinding.ActivityDetailExerciseBinding
 
 class DetailExerciseActivity : AppCompatActivity() {
+
+    private var _binding : ActivityDetailExerciseBinding? = null
+    private val binding get() = _binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_exercise)
+        _binding = ActivityDetailExerciseBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
 
+        val exerciseCategory = intent.getStringExtra(WeeklyExerciseActivity.EXERCISE)
+        val dayOfWeek = intent.getStringExtra(WeeklyExerciseActivity.DAY_OF_WEEK)
+
+        Log.e("MY_TAG","exercise Category: $exerciseCategory")
+        Log.e("MY_TAG","day of week: $dayOfWeek")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
